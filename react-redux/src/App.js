2ebe6d163ef23675c261c40dc04d5f7b3ff21817
vy3191 <venkyafhs@gmail.com>
2019-11-04 19:05:55 -0500

    Completed MVP getting quotes using axios get request

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
+import {connect} from 'react-redux';
 import Form from  './components/Form';
+import {getAllQuotes} from './actions/quotes';
+import Quote from './components/Quote';
 import './App.css';
 
-function App() {
+function App(props) {
+  useEffect( () => {
+     props.getAllQuotes();
+  },[]);
+ console.log('app-12', props.quotes)
   return (
     <div className="App">
       <h1>Job Searching Website</h1>
-      <Form />
+      {
+        (props.isLoading && props.quotes.length === 0) ?
+        <h1>Loadign quotes...</h1> :
+        (props.loaded && props.quotes.map( (quote, index) => {
+           return <Quote key={index} quote={quote} />
+        }))
+      }
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+   return {
+     quotes: state.quotes,
+     isLoading: state.isLoading,
+     loaded:state.loaded
+   }
+}
+const mapDispatchToProps = {
+   getAllQuotes
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
